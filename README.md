ac818499f80dda2ca69999380fca89e192d1baf7
Mikael Sundin <sundin.mikael@gmail.com>
2025-05-02 19:45:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # home_assistant_yaml_generators
-Yaml generators to batch generate some tasks
+Yaml generators to batch generate some yaml code.
+
+* Generate button to set input_text entity for shopping list.
+
+
+
+
---
+++ shopping_list_add_buttons/generate_yaml.py
@@ -0,0 +1,74 @@
+import sys
+import os
+
+
+def generate_button(value):
+    text_entity = "text.shopping_add_text"
+
+    return f"""
+  - show_name: true
+    show_icon: false
+    type: button
+    name: {value}
+    tap_action:
+      action: call-service
+      service: text.set_value
+      data:
+        value: {value}
+      target:
+        entity_id: {text_entity}
+    show_state: false
+    icon: mdi:checkbox-marked-circle-plus-outline
+    grid_options:
+      columns: 3
+      rows: 1
+    """.rstrip()
+
+def generate_heading(value):
+    return f"""
+  - type: heading
+    heading: {value}
+    heading_style: title"""
+
+
+def generate_yaml(input_file, output_file):
+    try:
+        #Use filename as our title
+        title =  os.path.splitext(input_file)[0].capitalize()
+        
+        #open the file
+        with open(input_file, 'r', encoding='utf-8') as file:
+            groceries = [line.strip() for line in file.readlines()]
+
+        #Grid card
+        output = "type: grid\ncards:"
+        
+        output += generate_heading(title)
+        for item in groceries:
+            if item:
+                output += generate_button(item)
+
+        #Remove newlines at beginning
+        output = output.lstrip("\n")
+        
+
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(output)
+
+    except FileNotFoundError:
+        print("Error: Input file not found. Please provide a valid file path.")
+
+# Example usage:
+if len(sys.argv) < 2:
+    print("Usage: python script.py <input_file>")
+else:
+    try:
+        os.makedirs("out", exist_ok=True)
+    except:
+        pass
+    
+    input_file = sys.argv[1]
+    output_file = f"out/{input_file}"
+    
+    generate_yaml(input_file, output_file)
+
